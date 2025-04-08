9aa8ff2b01b274618476276a87668fad25a363dd
fan <1508422424@qq.com>
2025-04-08 23:16:20 +0800

    增加downloadURL和updateURL

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import { defineCustomElement } from 'vue';
 import './style.css';
 import App from './App.vue';
-import { GM_getResourceText } from '$'
+import { GM_getResourceText, GM_info, GM_log } from '$'
 
-
+GM_log(GM_info.script.version)
 const BackTopAppElement = defineCustomElement(App, {
   styles: [GM_getResourceText('vant.index.css')]
 })
--- vite.config.js
+++ vite.config.js
@@ -15,21 +15,25 @@ export default defineConfig({
       userscript: {
         name: '一键返回顶部（EasyBackTop）',
         description: '为网页添加一个返回页面顶部的操作按钮，一键返回顶部，默认支持GitHub、v2ex、豆瓣电影这几个网站，但你可以通过“设置”添加更多需要支持的网站。',
-        icon: 'https://vitejs.dev/logo.svg',
-        namespace: 'npm/vite-plugin-monkey',
+        author: 'fanxq',
+        version: '1.0.1',
+        icon: 'https://cdn.jsdelivr.net/gh/fan0530/easy-back-top/src/assets/icon.svg',
+        namespace: 'https://github.com/fan0530',
         match: ['https://movie.douban.com/*', 'https://github.com/*', 'https://www.v2ex.com/*'],
         resource: {'vant.index.css': 'https://unpkg.com/vant@4.9.6/lib/index.css'},
-        "run-at": "document-end"
+        "run-at": "document-end",
+        downloadURL: 'https://gitee.com/fanxiqian/user-scripts/raw/master/easy-back-top/easy-back-top.user.js',
+        updateURL: 'https://gitee.com/fanxiqian/user-scripts/raw/master/easy-back-top/easy-back-top.user.js'
       },
       build: {
         externalGlobals: {
-          vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js').concat(
+          vue: cdn.unpkg('Vue', 'dist/vue.global.prod.js').concat(
             await util.fn2dataUrl(() => {
               // @ts-ignore
               window.Vue = Vue;
             }),
           ),
-          vant: cdn.jsdelivr('vant', 'lib/vant.min.js' )
+          vant: cdn.unpkg('vant', 'lib/vant.min.js' )
         },
         // externalResource: {
         //   'vant/lib/index.min.css': cdn.jsdelivr(),
